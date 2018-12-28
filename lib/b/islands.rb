e9f53b0846aed7ba1218c465e26bd60937e6377d
Jane Venter <janeanneventer@gmail.com>
2018-12-28 22:38:17 +0000

    first commit

---
+++ lib/b/islands.rb
@@ -0,0 +1,8 @@
+require "b/islands/version"
+
+module B
+  module Islands
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/b/islands/version.rb
@@ -0,0 +1,5 @@
+module B
+  module Islands
+    VERSION = "0.1.0"
+  end
+end
